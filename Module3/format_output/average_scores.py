c5a1c6f66e88df0d3e1f8c67b6c0c37941c9dffa
jsharpe13 <58717569+jsharpe13@users.noreply.github.com>
2020-06-06 20:02:51 -0500

    final push

--- Module3/format_output/average_scores.py
+++ Module3/format_output/average_scores.py
@@ -1,8 +1,35 @@
+"""
+Program: average_scores.py
+Author: Jacob Sharpe
+Last date modified: 6/06/2020
+
+The purpose of this program is to accept input on a student
+and their grades and output the average of 3 grades
+"""
+
+
 def average():
     score1 = input('input score 1: ')
+    score1 = float(score1)
     score2 = input('input score 2: ')
+    score2 = float(score2)
     score3 = input('input score 3: ')
+    score3 = float(score3)
 
     AvgScore = (score1 + score2 + score3) / 3
 
     return AvgScore
+
+
+if __name__ == '__main__':
+    last_name = input("enter student's last name: ")
+    first_name = input("enter student's first name: ")
+    age = input("enter student's age: ")
+    average_score = average()
+    print("{0}, {1} age: {2} average grade: {3:.2f}".format(last_name, first_name, age, average_score))
+
+# Input       Expected Output  Actual Output
+# 90,90,90          90.00         90.00
+# 38,97,62          65.66         65.67
+# 85,90,95          90.00         90.00
+# 'hello'           error          error
--- Module3/test/test_average_scores.py
+++ Module3/test/test_average_scores.py
@@ -5,7 +5,7 @@ from format_output import average_scores as avg
 
 
 class AverageTestCase(unittest.TestCase):
-    
+
     def test_average(self):
         with mock.patch('builtins.input', side_effect=[85, 90, 95]):
             assert avg.average() == 90
