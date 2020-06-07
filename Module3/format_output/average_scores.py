bc66139f2fc6d432b6ac76e6e7d4805f56a6ec2c
jsharpe13 <58717569+jsharpe13@users.noreply.github.com>
2020-06-06 19:11:28 -0500

    average function

--- Module3/format_output/average_scores.py
+++ Module3/format_output/average_scores.py
@@ -1,2 +1,8 @@
 def average():
-    pass
+    score1 = input('input score 1: ')
+    score2 = input('input score 2: ')
+    score3 = input('input score 3: ')
+
+    AvgScore = (score1 + score2 + score3) / 3
+
+    return AvgScore
